aaed031eb2062a30ed007e7b7d48103e85655767
Chuck Lui <luialex0@gmail.com>
2022-02-22 17:06:09 -0800

    have a bug in mock test

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,17 +1,30 @@
 "use strict";
 
-const {
-  shipProduct,
-} = require("./shipItApi");
+const AxiosMockAdapter = require("axios-mock-adapter");
+const axios = require("axios");
+const axiosMock = new AxiosMockAdapter(axios);
+const { shipProduct, SHIPIT_SHIP_URL } = require("./shipItApi");
 
+  // test("shipProduct", async function () {
+  //   const shipId = await shipProduct({
+  //     productId: 1000,
+  //     name: "Test Tester",
+  //     addr: "100 Test St",
+  //     zip: "12345-6789",
+  //   });
+  
+  //   expect(shipId).toEqual(expect.any(Number));
+  // });
 
-test("shipProduct", async function () {
-  const shipId = await shipProduct({
-    productId: 1000,
-    name: "Test Tester",
-    addr: "100 Test St",
-    zip: "12345-6789",
+  test("test about axios request", async function () {
+    axiosMock.onPost(`${SHIPIT_SHIP_URL}`)
+        .reply(200, { shipped: 1001 });
+  
+    const res = await shipProduct({
+      productId: 1000,
+      name: "Test Tester",
+      addr: "100 Test St",
+      zip: "12345-6789",
+    });
+    expect(res).toEqual({ shipped: 1001 });
   });
-
-  expect(shipId).toEqual(expect.any(Number));
-});
